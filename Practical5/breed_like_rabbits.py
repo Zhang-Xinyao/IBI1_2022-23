96312f6e827c1b49c8f913f5bee8753951538f96
张欣瑶 <zhangxinyao@zhangxinyaodeMacBook-Pro.local>
2023-03-14 22:10:33 +0800

    a

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -2,7 +2,7 @@ ans = 1
 for n in range(1,10):
  ans = ans * 2
  if (ans > 100):
-        print("The generation is",n)
+        print'The generation exceeding 100 is', n
         break
  print(ans)
 
