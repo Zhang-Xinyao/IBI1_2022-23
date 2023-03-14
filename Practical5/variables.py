ddabb5f2cdde67db127f27fb0a7824473ae61d65
Zhang-Xinyao <127190752+Zhang-Xinyao@users.noreply.github.com>
2023-03-14 15:43:09 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,1 +1,15 @@
-
+a=-3.19
+b=-118.24
+c=116.39
+d=a-b
+e=a-c
+d>e
+# The result is "True". d is greater than e so Rob travel further to Los Angeles.
+X=True
+Y=False
+W=X and Y
+print(W)
+#False
+Z=X or Y
+print(Z)
+#True
