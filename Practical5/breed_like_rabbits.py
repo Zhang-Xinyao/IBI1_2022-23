34af722ebf53ed37ebbdd20de37c5ab5d89e510e
张欣瑶 <zhangxinyao@zhangxinyaodeMacBook-Pro.local>
2023-03-15 15:30:40 +0800

    q

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,13 +1,13 @@
 #give enough loops,and "n" refers to the generation
 for n in range(1,100):
 #each generation has 2^n rabbits
- ans = 2**n
- if (ans > 100):
+	ans = 2**n
+	if (ans > 100):
 #when the "n" satisfies "ans>100",stop the loop and output this "n"
-        print'The generation exceeding 100 is', n
-        break
+        	print'The generation exceeding 100 is', n
+        	break
 #output the amount of rabbits in each generations
- print(ans)
+	print(ans)
 
 
 
