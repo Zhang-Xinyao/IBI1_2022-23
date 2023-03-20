ee1b5294761981e0c7df262fb93e38284ae0fb9b
张欣瑶 <zhangxinyao@zhangxinyaodeMacBook-Pro.local>
2023-03-20 11:32:14 +0800

    x

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,4 +1,5 @@
-#Repeat
+#How to calculate the first five values of the hexagon sequence
+#put the first five value into the formula and repeat the following commands
 for n in range(1,6):
 #The formula for the nth hexagonal number is h=2*n*(2*n-1)/2
  print(2*n*(2*n-1)/2)
