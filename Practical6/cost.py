006fa39f7f6d57c522b95856469bb457efd1f7ab
张欣瑶 <zhangxinyao@zhangxinyaodeMacBook-Pro.local>
2023-03-22 16:47:28 +0800

    2

--- Practical6/cost.py
+++ Practical6/cost.py
@@ -1,17 +1,25 @@
+#To draw a bar plot displaying the sorted distribution 
 import numpy as np
 import matplotlib.pyplot as plt
-
+#Define the number of horizontal cooridinates
 N=8
-costs=(1,8,15,7,5,14,43,40)
+olympic=['Los Angeles 1984','Seoul 1988','Barcelona 1992','Atlanta 1996','Sydney 2000','Athens 2003','Beijing 2008','London 2012']
+costs=[1,8,15,7,5,14,43,40]
+#get the list into array to sort the horizontal coordiantes in the order of the size of vertical coordinate
+costs=np.array(costs)
+olympic=np.array(olympic)
+inds=costs.argsort()
+sortedolympic=olympic[inds]
 ind=np.arange(N)
 width=0.35
-p1=plt.bar(ind,costs,width)
+p1=plt.bar(ind,sorted(costs),width)
 plt.ylabel('costs(in $ billions)')
 plt.title('Olympic costs in each contury')
-plt.xticks(ind,('Los Angeles 1984','Seoul 1988','Barcelona 1992','Atlanta 1996','Sydney 2000','Athens 2003','Beijing 2008','London 2012'))
+#set the coordinate scale
+plt.xticks(ind,sortedolympic)
 plt.yticks(np.arange(0,41,5))
 
 plt.show()
-
+#output the sorted list of costs
 costs=[1,8,15,7,5,14,43,40]
-print(costs)
+print(sorted(costs))
