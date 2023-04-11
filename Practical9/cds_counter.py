79ad60f4b13aeca1f1479983fc0f891be96c1d2e
张欣瑶 <zhangxinyao@zhangxinyaodeMacBook-Pro.local>
2023-04-11 14:45:48 +0800

    1

--- Practical9/cds_counter.py
+++ Practical9/cds_counter.py
@@ -1,1 +1,6 @@
-
+import re
+seq='ATGCAATCGACTACGATCTGAGAGGGCCTAA'
+count1=len(re.findall('^ATG.+TAA',seq))
+count2=len(re.findall('^ATG.+TAG',seq))
+count3=len(re.findall('^ATG.+TGA',seq))
+print(count1+count2+count3)
