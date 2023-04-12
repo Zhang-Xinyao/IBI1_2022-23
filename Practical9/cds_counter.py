275fd5b5519224334aaa1b95ac068570a4f5d229
张欣瑶 <zhangxinyao@zhangxinyaodeMacBook-Pro.local>
2023-04-12 15:53:23 +0800

    5

--- Practical9/cds_counter.py
+++ Practical9/cds_counter.py
@@ -1,8 +1,9 @@
 import re
 seq='ATGCAATCGACTACGATCTGAGAGGGCCTAA'
-count1=len(re.findall('^ATG.+TAA',seq))
-count2=len(re.findall('^ATG.+TAG',seq))
-count3=len(re.findall('^ATG.+TGA',seq))
+seq_count=seq.split('ATG')[1]
+count1=len(re.findall(r'TAA',seq_count))
+count2=len(re.findall(r'TAG',seq_count))
+count3=len(re.findall(r'TGA',seq_count))  
 #calculate how coding subsequence in this sequence ends with stop codons respectively
 print(count1+count2+count3)
-#calculate and output the number of total sequence 
+#calculate and output the number of total sequence  
