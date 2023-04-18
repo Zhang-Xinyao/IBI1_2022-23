49e795e36885e64adc1551d45e18e3b375b5578f
Zhang-Xinyao <127190752+Zhang-Xinyao@users.noreply.github.com>
2023-04-18 16:24:09 +0800

    Create mortgage_affordability_calculator.py

---
+++ Practical10/mortgage_affordability_calculator.py
@@ -0,0 +1,11 @@
+#define a function that can calculate the mortgage affordavility
+#"x" represents the salary and "y" represents the house value
+def calculator(x, y):
+#if you buy	a house	worth no more than five	times your annual salary
+#output "yes"
+#if more than five times 
+#output "no"
+    if x*5>=y:
+        print('Yes')
+    else:
+        print('No')
