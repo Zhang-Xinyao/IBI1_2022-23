2cdb9e94357da13919b5a8079253e052246e978b
张欣瑶 <zhangxinyao@zhangxinyaodeMacBook-Pro.local>
2023-04-19 15:38:44 +0800

    a

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,11 +1,14 @@
 #How to get the population of rabbits in each genration andknow at which genration the number of rabbit exceeds 100
 #give enough loops,and "n" refers to the generation
-for n in range(1,100):
+ans=2
+n=1
+while True:
 #each generation has 2^n rabbits
-	ans = 2**n
+	ans = 2*ans
+	n=n+1
 	if (ans > 100):
 #when the "n" satisfies "ans>100",stop the loop and output this "n"
-        	print('The generation exceeding 100 is', n)
+        	print('The generation exceeding 100 is', n,'th generation')
         	break
 #output the amount of rabbits in each generations
 	print(ans)
